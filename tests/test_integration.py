fdf4a4d5340d81b076eb19be3235584321210d89
Iago Suárez <iago_h92@hotmail.com>
2023-10-16 00:12:01 +0200

    Addressing PR comments

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,7 +1,10 @@
+import logging
 import unittest
 from collections import namedtuple
+from os.path import splitext
 
 import cv2
+import matplotlib.pyplot as plt
 import torch.cuda
 from kornia import image_to_tensor
 from omegaconf import OmegaConf
@@ -18,6 +21,11 @@ from gluefactory.settings import root
 from gluefactory.utils.image import ImagePreprocessor
 from gluefactory.utils.tensor import map_tensor
 from gluefactory.utils.tools import set_seed
+from gluefactory.visualization.viz2d import (
+    plot_color_line_matches,
+    plot_images,
+    plot_matches,
+)
 
 
 def create_input_data(cv_img0, cv_img1, device):
@@ -32,10 +40,6 @@ def create_input_data(cv_img0, cv_img1, device):
         else torch.from_numpy(t)[None].to(device),
     )
     return data
-
-
-def squeeze_if_tensor(t):
-    return torch.squeeze(t, dim=0) if isinstance(t, Tensor) else t
 
 
 ExpectedResults = namedtuple("ExpectedResults", ("num_matches", "prec3px", "h_error"))
@@ -57,12 +61,15 @@ class TestIntegration(unittest.TestCase):
         ),
     ]
 
+    IMSHOWS = False
+
     @parameterized.expand(methods_to_test)
+    @torch.no_grad()
     def test_real_homography(self, conf_file, estimator, exp_results):
         set_seed(0)
         model_path = root / "gluefactory" / "configs" / conf_file
-        img_path0 = root / "docs" / "boat1.png"
-        img_path1 = root / "docs" / "boat2.png"
+        img_path0 = root / "assets" / "boat1.png"
+        img_path1 = root / "assets" / "boat2.png"
         h_gt = torch.tensor(
             [
                 [0.85799, 0.21669, 9.4839],
@@ -77,7 +84,9 @@ class TestIntegration(unittest.TestCase):
         cv_img0, cv_img1 = cv2.imread(str(img_path0)), cv2.imread(str(img_path1))
         data = create_input_data(cv_img0, cv_img1, device)
         pred = gs(data)
-        pred = map_tensor(pred, squeeze_if_tensor)
+        pred = map_tensor(
+            pred, lambda t: torch.squeeze(t, dim=0) if isinstance(t, Tensor) else t
+        )
         data["H_0to1"] = h_gt.to(device)
         data["H_1to0"] = torch.linalg.inv(h_gt).to(device)
 
@@ -93,28 +102,31 @@ class TestIntegration(unittest.TestCase):
             ),
         }
 
-        print(results)
+        logging.info(results)
         self.assertGreater(results["num_matches"], exp_results.num_matches)
         self.assertGreater(results["prec@3px"], exp_results.prec3px)
         self.assertLess(results["H_error_ransac"], exp_results.h_error)
 
-        # pred = batch_to_numpy(pred)
-        # # pred = map_tensor(pred, lambda t: t[0].detach().cpu().numpy())
-        # kp0, kp1 = pred['keypoints0'], pred['keypoints1']
-        # m0 = pred['matches0']
-        # valid0 = m0 != -1
-        # kpm0, kpm1 = kp0[valid0], kp1[m0[valid0]]
-        #
-        # plot_images([cv_img0, cv_img1])
-        # plot_matches(kpm0, kpm1, a=0.0)
-        # plt.savefig(f'point_matches.png')
-        #
-        # lines0, lines1 = pred['lines0'], pred['lines1']
-        # lm0 = pred['line_matches0']
-        # lvalid0 = lm0 != -1
-        # linem0, linem1 = lines0[lvalid0], lines1[lm0[lvalid0]]
-        #
-        # plot_images([cv_img0, cv_img1])
-        # plot_color_line_matches([linem0, linem1])
-        # plt.savefig(f'line_matches.svg')
-        # plt.show()
+        if self.IMSHOWS:
+            pred = map_tensor(
+                pred, lambda t: t.cpu().numpy() if isinstance(t, Tensor) else t
+            )
+            kp0, kp1 = pred["keypoints0"], pred["keypoints1"]
+            m0 = pred["matches0"]
+            valid0 = m0 != -1
+            kpm0, kpm1 = kp0[valid0], kp1[m0[valid0]]
+
+            plot_images([cv_img0, cv_img1])
+            plot_matches(kpm0, kpm1, a=0.0)
+            plt.savefig(f"{splitext(conf_file)[0]}_point_matches.svg")
+
+            if "lines0" in pred and "lines1" in pred:
+                lines0, lines1 = pred["lines0"], pred["lines1"]
+                lm0 = pred["line_matches0"]
+                lvalid0 = lm0 != -1
+                linem0, linem1 = lines0[lvalid0], lines1[lm0[lvalid0]]
+
+                plot_images([cv_img0, cv_img1])
+                plot_color_line_matches([linem0, linem1])
+                plt.savefig(f"{splitext(conf_file)[0]}_line_matches.svg")
+            plt.show()
