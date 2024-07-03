a4db845890d8f941d84b05eb97093eb03f35b51d
Felix Möller <felix.moeller4@gmail.com>
2024-07-03 15:57:23 +0200

    Refactor deeplsd_utils

--- gluefactory/models/deeplsd_inference.py
+++ gluefactory/models/deeplsd_inference.py
@@ -11,7 +11,7 @@ from torch import nn
 from gluefactory.models.backbones.vgg_unet import VGGUNet
 from gluefactory.models.base_model import BaseModel
 from gluefactory.models.lines.line_refinement import filter_outlier_lines, merge_lines
-from gluefactory.utils.deeplsd_utils import preprocess_angle
+from gluefactory.models.lines.line_utils import preprocess_angle
 
 
 class DeepLSD(BaseModel):
