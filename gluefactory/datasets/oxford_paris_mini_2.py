6b69295db544d481a055ac9e49d4d4878656ec68
Raphael Kreft <rkreft@cnb-d102-23.inf.ethz.ch>
2024-09-18 09:49:32 +0200

    Final Fixes to one-view oxparis dataset. Add train configs for one view oxparis.

--- gluefactory/datasets/oxford_paris_mini_2.py
+++ gluefactory/datasets/oxford_paris_mini_2.py
@@ -17,7 +17,7 @@ from gluefactory.utils.tensor import batch_to_device
 logger = logging.getLogger(__name__)
 
 
-class OxfordParisMini(BaseDataset):
+class OxfordParisMini2(BaseDataset):
     """
     Subset of the Oxford Paris dataset as defined here: https://cmp.felk.cvut.cz/revisitop/
     Supports loading groundtruth and only serves images for that gt exists.
@@ -29,7 +29,7 @@ class OxfordParisMini(BaseDataset):
     """
 
     default_conf = {
-        "data_dir": "revisitop1m/jpg",  # as subdirectory of DATA_PATH(defined in settings.py)
+        "data_dir": "revisitop1m_POLD2/jpg",  # as subdirectory of DATA_PATH(defined in settings.py)
         "grayscale": False,
         "train_batch_size": 2,  # prefix must match split
         "test_batch_size": 1,
@@ -46,12 +46,13 @@ class OxfordParisMini(BaseDataset):
             "check_exists": True,
             "point_gt": {
                 "data_keys": ["superpoint_heatmap"],
+                "use_score_heatmap": True,
             },
             "line_gt": {
                 "data_keys": ["deeplsd_distance_field", "deeplsd_angle_field"],
             },
         },
-        "img_list": "gluefactory/datasets/oxford_paris_images.txt",  # path to image list containing images we want to use for this dataset to be consitent (given from repo root)
+        "img_list": "gluefactory/datasets/oxford_paris_images.txt",  # img list path from repo root -> use checked in file list, it is similar to pold2 file 
         "rand_shuffle_seed": None,  # seed to randomly shuffle before split in train and val
         "val_size": 10,  # size of validation set given
         "train_size": 100000,
@@ -70,6 +71,7 @@ class OxfordParisMini(BaseDataset):
         train_images = images[: conf.train_size]
         val_images = images[conf.train_size: conf.train_size + conf.val_size]
         self.images = {"train": train_images, "val": val_images, "test": images, "all": images}
+        print(f"DATASET OVERALL(NO-SPLIT) IMAGES: {len(images)}")
 
 
     def download_oxford_paris_mini(self):
@@ -112,51 +114,52 @@ class OxfordParisMini(BaseDataset):
 
 
 class _Dataset(torch.utils.data.Dataset):
-    def __init__(self, conf, image_paths: list[str], split):
+    def __init__(self, conf, image_sub_paths: list[str], split):
         self.split = split
         self.conf = conf
         self.grayscale = bool(conf.grayscale)
         # self.conf is set in superclass
         # set img preprocessor
         if self.conf.reshape is not None:
-            reshape_preprocessing = f"resize: {self.conf.reshape}"
+            reshape_preprocessing = {"resize": self.conf.reshape}
             self.preprocessor = ImagePreprocessor(reshape_preprocessing)
 
-        # select split scenes
         self.img_dir = DATA_PATH / conf.data_dir
 
         # Extract image paths
-        self.image_paths = [Path(i) for i in image_paths]
+        self.image_sub_paths = image_sub_paths #[Path(i) for i in image_sub_paths]
 
         # making them relative for system independent names in export files (path used as name in export)
-        if len(self.image_paths) == 0:
+        if len(self.image_sub_paths) == 0:
             raise ValueError(f"Could not find any image in folder: {self.img_dir}.")
-        logger.info(f"NUMBER OF IMAGES: {len(self.image_paths)}")
+        logger.info(f"NUMBER OF IMAGES: {len(self.image_sub_paths)}")
         # Load features
         if conf.load_features.do:
             # filter out where missing groundtruth
             new_img_path_list = []
-            for img_path in self.image_paths:
+            for img_path in self.image_sub_paths:
                 if not self.conf.load_features.check_exists:
                     new_img_path_list.append(img_path)
                     continue
                 # perform checks TODO: Add offsets etc?
-                img_folder = img_path.parent
+                full_artificial_img_path = Path(self.img_dir / img_path) 
+                img_folder = full_artificial_img_path.parent / full_artificial_img_path.stem
                 keypoint_file = img_folder / "keypoint_scores.npy"
-                af_file = img_folder / "af_scores.npy"
-                df_file = img_folder / "df_scores.npy"
+                af_file = img_folder / "angle.jpg"
+                df_file = img_folder / "df.jpg"
 
                 if keypoint_file.exists() and af_file.exists() and df_file.exists():
                     new_img_path_list.append(img_path)
 
-            self.image_paths = new_img_path_list
-            logger.info(f"NUMBER OF IMAGES WITH GT: {len(self.image_paths)}")
+            self.image_sub_paths = new_img_path_list
+            logger.info(f"NUMBER OF IMAGES WITH GT: {len(self.image_sub_paths)}")
 
-    def _read_image(self, path, enforce_batch_dim=False):
+    def _read_image(self, img_folder_path, enforce_batch_dim=False):
         """
         Read image as tensor and puts it on device
         """
-        img = load_image(path, grayscale=self.grayscale)
+        img_path = img_folder_path / 'base_image.jpg'
+        img = load_image(img_path, grayscale=self.grayscale)
         if enforce_batch_dim:
             if img.ndim < 4:
                 img = img.unsqueeze(0)
@@ -165,69 +168,84 @@ class _Dataset(torch.utils.data.Dataset):
             img = img.to(self.conf.device)
         return img
 
-    def _read_groundtruth(self, image_path):
+    def _read_groundtruth(self, image_folder_path, original_img_size):
         """
         Reads groundtruth for points and lines from respective files
         We can assume that gt files are existing at this point->filtered in init!
 
-        image_path: path to image as relative to base directory(self.img_path)
+        image_folder_path: full image folder path to get the gt data from
+        original_img_size: format w,h original image size to be able to create heatmaps.
         """
+        w, h = original_img_size
         features = {}
-        kp_file = image_path.parent / "keypoints.npy"
-        kps_file = image_path.parent / "keypoint_scores.npy"
+        kp_file = image_folder_path / "keypoints.npy"
+        kps_file = image_folder_path / "keypoint_scores.npy"
 
         # Load keypoints and scores
-        kp = torch.from_numpy(np.load(kp_file)).to(dtype=torch.float32)
-        if self.conf.reshape is not None:
-            kp = self.preprocessor(kp)
-        features["keypoints"] = kp
+        orig_kp = torch.from_numpy(np.load(kp_file)).to(dtype=torch.float32)
+        integer_kp = torch.round(orig_kp).to(dtype=torch.int)
 
         kps = torch.from_numpy(np.load(kps_file)).to(dtype=torch.float32)
+       
+        # TODO: Do we need keypoints/scores?
+        #features["keypoints"] = orig_kp
+        #features["keypoint_scores"] = kps
+
+        heatmap = np.zeros((w, h))
+        coordinates = np.transpose(integer_kp)
+        if self.conf.load_features.point_gt.use_score_heatmap:
+            heatmap[coordinates[0], coordinates[1]] = kps
+        else:
+            heatmap[coordinates[0], coordinates[1]] = 1.
+        heatmap = torch.from_numpy(heatmap).to(dtype=torch.float32)
+       
         if self.conf.reshape is not None:
-            kps = self.preprocessor(kps)
-        features["keypoint_scores"] = kps
-        features = batch_to_device(features, self.conf.device)
+            heatmap = self.preprocessor(heatmap)['image']
+       
+        features[self.conf.load_features.point_gt.data_keys[0]] = heatmap
+        
+        #features = batch_to_device(features, self.conf.device)
 
 
         # Load pickle file for DF max and min values
-        with open(image_path.parent / "values.pkl", "rb") as f:
+        with open(image_folder_path / "values.pkl", "rb") as f:
             values = pickle.load(f)
 
         # Load DF
-        df_img = read_image(image_path.parent / "df.jpg", True)
+        df_img = read_image(image_folder_path / "df.jpg", True)
         df_img = df_img.astype(np.float32) / 255.0
         df_img *= values["max_df"]
 
         # Load AF
-        af_img = read_image(image_path.parent / "angle.jpg", True)
+        af_img = read_image(image_folder_path / "angle.jpg", True)
         af_img = af_img.astype(np.float32) / 255.0
         af_img *= np.pi
 
         # Get closest point to line for each pixel
         # offset = self.df_and_angle_to_offset(df_img, af_img)
-        ofx_img = read_image(image_path.parent / "offset_x.jpg", True)
-        ofx_img = ofx_img.astype(np.float32) / 255.0
-        ofy_img = read_image(image_path.parent / "offset_y.jpg", True)
-        ofy_img = ofy_img.astype(np.float32) / 255.0
-        offset = np.stack((ofx_img, ofy_img), axis=-1)
-        offset = offset * values["max_offset"]
-        offset = offset + values["min_offset"]
+        #ofx_img = read_image(image_folder_path / "offset_x.jpg", True)
+        #ofx_img = ofx_img.astype(np.float32) / 255.0
+        #ofy_img = read_image(image_folder_path / "offset_y.jpg", True)
+        #ofy_img = ofy_img.astype(np.float32) / 255.0
+        #offset = np.stack((ofx_img, ofy_img), axis=-1)
+        #offset = offset * values["max_offset"]
+        #offset = offset + values["min_offset"]
 
         df = torch.from_numpy(df_img).to(dtype=torch.float32)
         if self.conf.reshape is not None:
-            df = self.preprocessor(df)
-        features[self.conf.load_features.line_gt[0]] = df
+            df = self.preprocessor(df)['image']
+        features[self.conf.load_features.line_gt.data_keys[0]] = df
         af = torch.from_numpy(af_img).to(dtype=torch.float32)
         if self.conf.reshape is not None:
-            af = self.preprocessor(af)
-        features[self.conf.load_features.line_gt[1]] = af
+            af = self.preprocessor(af)['image']
+        features[self.conf.load_features.line_gt.data_keys[1]] = af
 
-        offset = torch.from_numpy(offset).to(dtype=torch.float32)
-        if self.conf.reshape is not None:
-            offset = self.preprocessor(offset)
-        features["offset"] = offset
+        #offset = torch.from_numpy(offset).to(dtype=torch.float32)
+        #if self.conf.reshape is not None:
+        #    offset = self.preprocessor(offset)['image']
+        #features["offset"] = offset
 
-        features = batch_to_device(features, self.conf.device)
+        #features = batch_to_device(features, self.conf.device)
 
         return features
 
@@ -235,18 +253,19 @@ class _Dataset(torch.utils.data.Dataset):
         """
         Dataloader is usually just returning one datapoint by design. Batching is done in Loader normally.
         """
-        path = self.image_paths[idx]
-        img = self._read_image(self.img_dir / path)
+        full_artificial_img_path = self.img_dir / self.image_sub_paths[idx]
+        folder_path = full_artificial_img_path.parent / full_artificial_img_path.stem
+        img = self._read_image(folder_path)
         data = {
-            "name": str(path),
+            "name": str(folder_path / 'base_image.jpg'),
             **self.preprocessor(img),
-        }  # add metadata, like transform, image_size etc...
+        }  # keys: 'name', 'scales', 'image_size', 'transform', 'original_image_size', 'image'
         if self.conf.load_features.do:
-            gt = self._read_groundtruth(path)
+            gt = self._read_groundtruth(folder_path, data["original_image_size"])
             data = {**data, **gt}
 
         return data
 
 
     def __len__(self):
-        return len(self.image_paths)
+        return len(self.image_sub_paths)
