02374770ac8fce697662f5a45c1af19b49636016
raphael <rkreft@ethz.ch>
2024-10-02 00:34:23 +0200

    add minor improvements to benchmark script

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -2,11 +2,12 @@
 
 # script params
 conf="0"
+out_folder_path="0"
 use_extended_bm=0
 
 # List benchmarks to run
 LIST_OF_NORMAL_BM_FILES=("hpathes" "megadepth1500")
-LIST_OF_EXTENDED_BM_FILES=("hpatches_extended" "megadepth1500")
+LIST_OF_EXTENDED_BM_FILES=("hpatches_extended" "megadepth1500_extended")
 LIST_OF_COMMON_BM_FILES=()
 
 # user specific settings
@@ -25,32 +26,55 @@ while [[ $# -gt 0 ]]; do
         echo "Using Configuration: $conf."
         shift
       else
-        echo "Error: Count requires a value."
+        echo "Error: Conf requires a value."
+        exit 1
+      fi
+      ;;
+    -o|--out)
+      if [[ -n "$2" ]]; then
+        out_folder_path="$2"
+        echo "Using Outputfolder: $out_folder_path."
+        shift
+      else
+        echo "Error: name requires a value."
         exit 1
       fi
       ;;
     *)
       echo "Unknown option: $1"
-      echo "Usage: $0 -c|--conf <config_file> [-wl|--withlines]"
+      echo "Usage: $0 -c|--conf <config_file> -n|--name <name_used_as_outfolder_name> [-wl|--withlines]"
       exit 1
       ;;
   esac
   shift
 done
 
+# check parameters
 if [ "$conf" = "0" ]; then
     echo "Path to config is necessary!"
-    echo "Usage: $0 -c|--conf <config_file> [-wl|--withlines]"
+    echo "Usage: $0 -c|--conf <config_file> -n|--name <name_used_as_outfolder_name> [-wl|--withlines]"
     exit 1
 fi
+if [ "$out_folder_path" = "0" ]; then
+    echo "Path to outputfolder is necessary!"
+    echo "Usage: $0 -c|--conf <config_file> -n|--name <name_used_as_outfolder_name> [-wl|--withlines]"
+    exit 1
+fi
+
+# create directory if not existing
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir "$out_folder_path"
+fi
+
 
 run_benchmark() {
   file_name=$1
   config_name=$2
+  out_folder_path=$3
   out_file_name=$1_$(date '+%Y_%m_%d').txt
 
   echo ">>> Run benchmark $file_name, store output to: $out_file_name"
-  #python -m gluefactory.eval."$file_name" --conf="$config_name" --overwrite > "$out_file_name"
+  #python -m gluefactory.eval."$file_name" --conf="$config_name" --overwrite > "$out_folder_path/$out_file_name"
 }
 
 prompt_continue() {
@@ -69,6 +93,7 @@ echo "-->Running benchmarks<--"
 echo "-using config: $conf"
 echo "-set repo: $path_to_repo"
 echo "-set env: $environment"
+echo "-out-folder: $out_folder_path"
 
 prompt_continue
 
@@ -92,13 +117,13 @@ if [ $use_extended_bm -eq 1 ]; then
   echo "> Run extended benchmarks... I hope the model returns lines, otherwise I will fail xD"
   for i in "${LIST_OF_EXTENDED_BM_FILES[@]}"
   do
-     run_benchmark "$i" "$conf"
+     run_benchmark "$i" "$conf" "$out_folder_path"
   done
  else
   echo "> Run extended benchmarks... I hope the model returns lines, otherwise I will fail xD"
   for i in "${LIST_OF_NORMAL_BM_FILES[@]}"
   do
-     run_benchmark "$i" "$conf"
+     run_benchmark "$i" "$conf" "$out_folder_path"
   done
 fi
 
