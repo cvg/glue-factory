3727164a26ba94330f812a53496e2cc2cf55f4bf
raphael <rkreft@ethz.ch>
2024-11-10 01:06:35 +0100

    cleanup repo: remove old files. mark settingscopy as backup

--- gluefactory/settings.py
+++ gluefactory/settings.py
@@ -1,10 +1,7 @@
 from pathlib import Path
 
-cluster_team_folder = Path(
-    "/local/home/rkreft/shared_team_folder"
-)  # cluster team folder for 3dv
 root = Path(__file__).parent.parent  # top-level directory
-p = Path("/local/home/Point-Line/")
+p = Path("/local/home/Point-Line/") # cluster team folder for 3dv
 DATA_PATH = p / "data"  # datasets and pretrained weights
 TRAINING_PATH = p / "outputs/training/"  # training checkpoints
 EVAL_PATH = p / "outputs/results/"  # evaluation results
