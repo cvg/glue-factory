cc1d81b84fce4d32b432d3c25dbfdb4366c568ae
hardik01shah <hardik01shah@gmail.com>
2024-11-12 10:42:44 +0100

    Update settings.py root path to remove symlink

--- gluefactory/settings.py
+++ gluefactory/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 cluster_team_folder = Path(
-    "/local/home/rkreft/shared_team_folder"
+    "/local/home/Point-Line/"
 )  # cluster team folder for 3dv
 root = Path(__file__).parent.parent  # top-level directory
 DATA_PATH = cluster_team_folder / "data"  # datasets and pretrained weights
