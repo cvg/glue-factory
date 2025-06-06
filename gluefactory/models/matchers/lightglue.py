fc4b9021b11d7ecace70496ed15b0b5b650bde65
Phil26AT <phil.lindenberger@gmail.com>
2025-06-06 13:11:18 +0200

    Update custom_fwd to be compatible iwth latest torch versions.

--- gluefactory/models/matchers/lightglue.py
+++ gluefactory/models/matchers/lightglue.py
@@ -18,7 +18,7 @@ FLASH_AVAILABLE = hasattr(F, "scaled_dot_product_attention")
 torch.backends.cudnn.deterministic = True
 
 
-@torch.cuda.amp.custom_fwd(cast_inputs=torch.float32)
+@torch.amp.custom_fwd(cast_inputs=torch.float32, device_type="cuda")
 def normalize_keypoints(
     kpts: torch.Tensor, size: Optional[torch.Tensor] = None
 ) -> torch.Tensor:
