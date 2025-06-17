eba49ea709cdc5bf1447e72c8f0503b9130ee5e5
Phil26AT <phil.lindenberger@gmail.com>
2025-06-17 16:25:48 +0200

    Add thirdparty path to settings

--- gluefactory/settings.py
+++ gluefactory/settings.py
@@ -4,5 +4,6 @@ root = Path(__file__).parent.parent  # top-level directory
 DATA_PATH = root / "data/"  # datasets and pretrained weights
 TRAINING_PATH = root / "outputs/training/"  # training checkpoints
 EVAL_PATH = root / "outputs/results/"  # evaluation results
+THIRD_PARTY_PATH = root / "third_party/"  # third-party libraries
 
 ALLOW_PICKLE = False  # allow pickle (e.g. in torch.load)
