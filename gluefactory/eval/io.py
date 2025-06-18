e39188995b65a1e1e5c37da6c8de24c93e475a04
Phil26AT <phil.lindenberger@gmail.com>
2025-06-18 12:44:03 +0200

    Resolve config interpolation in train/eval

--- gluefactory/eval/io.py
+++ gluefactory/eval/io.py
@@ -49,6 +49,7 @@ def parse_eval_args(benchmark, args, configs_path, default=None):
     if args.conf:
         conf_path = parse_config_path(args.conf, configs_path)
         custom_conf = OmegaConf.load(conf_path)
+        OmegaConf.resolve(custom_conf)
         conf = extract_benchmark_conf(OmegaConf.merge(conf, custom_conf), benchmark)
         args.tag = (
             args.tag if args.tag is not None else conf_path.name.replace(".yaml", "")
